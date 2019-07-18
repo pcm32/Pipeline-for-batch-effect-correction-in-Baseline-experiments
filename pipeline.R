39093795565388aa7b84fa031b8055d8dfb40892
Guillaume Heger <guillaume.heger@gmail.com>
2019-07-18 18:18:13 +0100

    cleaned code

--- pipeline.R
+++ pipeline.R
@@ -10,23 +10,14 @@ library(RUVSeq)
 library(batchelor)
 
 list_experiments<-function(directory){
-  experiments<-list()
-  for(file in dir(directory)){
-    experiments[[file %>% str_split("-") %>% extract2(1) %>% extract(2:3) %>% paste(collapse="")]] <- get(load(paste0(directory,'/',file)))$rnaseq
-  }
-  return(experiments)
+  directory %>% dir %>% map(~get(load(paste0(directory,'/',.x)))$rnaseq) %>% 
+    set_names(directory %>% dir %>% str_split('-') %>% map(~.x[2:3] %>% paste(collapse='')) %>% unlist)
 }
 
 remove_isolated_experiments<-function(experiments,factor){
   batch<-experiments %>% imap(~.y %>% rep(dim(.x)[2])) %>% unlist(use.names=FALSE)
   group<-experiments %>% map(~.[[factor]]) %>% unlist(use.names=FALSE)
-  # batch<-NULL; group<-NULL
-  # for(i in experiments %>% seq_along){
-  #   batch%<>%c(names(experiments)[[i]] %>% rep(dim(experiments[[i]])[2]))
-  #   group%<>%c(experiments[[i]][[factor]])
-  # }; batch%<>%factor
   groups <- group %>% split(batch)
-  
   intersections<-NULL
   for(i in groups){
     for(j in groups){
@@ -34,22 +25,11 @@ remove_isolated_experiments<-function(experiments,factor){
     }
   }
   intersections%<>%matrix(length(groups))%<>%set_colnames(names(groups))%<>%set_rownames(names(groups))
-  
   intersections %>% graph_from_adjacency_matrix %>% plot
-  
   experiments[rownames(intersections)[rowSums(intersections!=0)<=1]]<-NULL
-  
   batch<-experiments %>% imap(~.y %>% rep(dim(.x)[2])) %>% unlist(use.names=FALSE)
   group<-experiments %>% map(~.[[factor]]) %>% unlist(use.names=FALSE)
-  
-  # batch<-NULL; group<-NULL
-  # for(i in experiments %>% seq_along){
-  #   batch%<>%c(names(experiments)[[i]] %>% rep(dim(experiments[[i]])[2]))
-  #   group%<>%c(experiments[[i]][[factor]])
-  # }; batch%<>%factor
-  
   groups <- group %>% split(batch)
-  
   intersections<-NULL
   for(i in groups){
     for(j in groups){
@@ -57,7 +37,6 @@ remove_isolated_experiments<-function(experiments,factor){
     }
   }
   intersections%<>%matrix(length(groups))%<>%set_colnames(names(groups))%<>%set_rownames(names(groups))
-  
   intersections %>% graph_from_adjacency_matrix %>% plot
   return(experiments)
 }
@@ -66,22 +45,13 @@ merge_experiments<-function(experiments, log=TRUE, filter=TRUE){
   genes<-experiments %>% map(rownames)
   common.genes<-genes %>% purrr::reduce(intersect)
   filtered.genes<-genes %>% map(setdiff %>% partial(y=common.genes))
-  
   data<-experiments %>% map(~.x@assays$data$counts %>% extract(rownames(.x)%in%common.genes,)) %>% purrr::reduce(cbind)
   batch<-experiments %>% imap(~.y %>% rep(ncol(.x))) %>% unlist(use.names=FALSE) %>% factor
-  
-  # data<-NULL;batch<-NULL
-  # for(i in experiments %>% seq_along){
-  #   experiments[[i]]->exp
-  #   data%<>%cbind(exp %>% assays %>% use_series(counts) %>% extract(rownames(exp)%in%common.genes,))
-  #   batch%<>%c(names(experiments)[[i]] %>% rep(dim(exp)[2]))
-  # }; batch%<>%factor
   if(filter){
     filter <- data %>% t %>% data.frame %>% split(batch) %>% map(~colSums(.)!=0) %>% purrr::reduce(`&`)
     data%<>%extract(filter,)
   }
   if(log) data%<>%log1p
-  # data%<>%correct.batch.effect(batch,method,model,log,model.data=if(is.null(model)) NULL else model.frame(model,vars),k=k)
   return(SummarizedExperiment(
     assays=if(log) list(log_counts=data) else list(counts=data),
     colData=experiments %>% map(colData) %>% smartbind(list=.) %>% set_rownames(experiments %>% map(colnames) %>% unlist) %>% cbind(batch),
